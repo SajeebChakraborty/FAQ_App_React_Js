918b6607143b6a3ace726abe3673e73448ce212b
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2022-12-14 23:37:09 +0600

    dummy data inserted

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const faqsData = [
+
+    {
+
+
+        id:1,
+        title: 'How can we reach you?',
+        desc: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
+
+
+
+
+    },
+    {
+
+
+        id:2,
+        title: 'when you are available?',
+        desc: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
+
+
+
+
+    },
+    {
+
+
+        id:3,
+        title: 'Where we can find you?',
+        desc: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
+
+
+
+
+    },
+
+
+
+
+]
