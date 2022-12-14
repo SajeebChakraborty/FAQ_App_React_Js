174683855adc28fed5602c2883503e7a1b484e58
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2022-12-15 01:03:05 +0600

    styling faqs component

--- src/components/FAQS.js
+++ src/components/FAQS.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import style from './faqs.module.css';
 
 import {data} from './data.js';
 import FAQ from './FAQ';
@@ -8,17 +9,23 @@ export default function FAQS() {
   const [faqs,setFaqs]=useState(data);
 
   return (
-    <div>
-      
-        {faqs.map((faq)=> (
 
-            <FAQ key={faq.id} {...faq}/>
+    <main className={style.container}>
 
-            
+        <section className={style.faqs}>
+
+            <h1>FAQs</h1>
+        
+            {faqs.map((faq)=> (
+
+                <FAQ key={faq.id} {...faq}/>
+
+                
 
 
-        ))}
+            ))}
 
-    </div>
+        </section>
+    </main>
   )
 }
